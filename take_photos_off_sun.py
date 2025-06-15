9eefcefc42a7949e524e330de126d0572e0e2360
Katie Harrington <kharrington@anl.gov>
2025-06-15 23:33:31 +0000

    reorg code

--- take_photos_off_sun.py
+++ take_photos_off_sun.py
@@ -6,6 +6,22 @@ import gphoto2 as gp
 import ephem
 from camera import CloudCamera, logger
 
+def get_file_from_now(shutter_speed, basedir='tmp'):
+    """get file from the current time. creates 5 digit ctime 
+    folders for the files as well 
+    """
+    now = int(time.time())
+    dir = str(now)[:5]
+
+    target = os.path.join(basedir, dir )
+    if not os.path.exists(target):
+        os.mkdir(target)
+    target = os.path.join(target, now)
+
+    x = shutter_speed.replace("/", "d").replace(".", "p")
+    target = f"{target}_s_{x}"
+
+    return target
 
 site = ephem.Observer()
 site.long = "-67.787925"
@@ -15,7 +31,6 @@ site.elev = 5188 #meters, about
 camera = CloudCamera()
 camera.set_image_format('Medium Fine JPEG')
 wait_time = 15
-
 
 while True:
     site.date  = ephem.Date(dt.datetime.now())
@@ -33,11 +48,9 @@ while True:
         speed = '1/500'  
 
     camera.set_shutterspeed(speed)
-    target = os.path.join('tmp', f"{int(time.time())}" )
-    x = speed.replace("/", "d").replace(".", "p")
-    target = f"{target}_s_{x}"
-    print(site.date, sun_el, speed, target)        
+    target = get_file_from_now(speed)
     
+    print(site.date, sun_el, speed, target)        
     camera.take_photo(target)
 
     time.sleep(wait_time*60)
